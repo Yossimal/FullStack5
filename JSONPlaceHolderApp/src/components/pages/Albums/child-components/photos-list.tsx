592627a8a144adb7fad9dd14710a19cdce9ae5e7
AvielBerko <9155988@gmail.com>
2023-05-23 14:21:30 +0300

    album page refactoring

---
+++ JSONPlaceHolderApp/src/components/pages/Albums/child-components/photos-list.tsx
@@ -0,0 +1,61 @@
+import { ListGroupItem, Card, Button, Row, Col } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Album from "../../../../lib/data/dataObjects/Album";
+import { useEffect, useState } from "react";
+import Photo from "../../../../lib/data/dataObjects/Photo";
+
+type PhotosItemProps = {
+    album: Album;
+    showPhotos: Boolean;
+};
+
+export default function PhotosList({ album, showPhotos }: PhotosItemProps) {
+    if (!album) return <></>;
+
+    const [photos, setPhotos] = useState<Photo[]>([]);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+
+    const fetchPhotos = async () => {
+        try {
+            album.photos(page).then((newPhotos) => {
+                if (newPhotos.length === 0) {
+                    setHasMore(false);
+                }
+                setPhotos((prevPhotos) => [...prevPhotos, ...newPhotos]);
+            });
+
+            // Update page number and check if there is more data available
+            setPage((prevPage) => prevPage + 1);
+        } catch (error) {
+            console.error("Error fetching albums:", error);
+        }
+    };
+
+    useEffect(() => {
+        showPhotos && fetchPhotos();
+      }, [showPhotos]);
+
+    return (
+        <InfiniteScroll
+            dataLength={photos.length}
+            next={fetchPhotos}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={<p>No more photos to load.</p>}
+        >
+            <Row>
+                {photos.map((photo) => (
+                    <Col key={photo.id} md={4}>
+                        <img
+                            src={photo.url}
+                            alt={photo.title}
+                            className="img-fluid"
+                        />
+                        <h4>{photo.title}</h4>
+                    </Col>
+                ))}
+            </Row>
+        </InfiniteScroll>
+    );
+}
