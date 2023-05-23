a0b577056d45cbb5aab475dc8181773301388eb0
AvielBerko <9155988@gmail.com>
2023-05-23 14:21:18 +0300

    album page refactoring

--- JSONPlaceHolderApp/src/components/pages/Albums/child-components/albums-item.tsx
+++ JSONPlaceHolderApp/src/components/pages/Albums/child-components/albums-item.tsx
@@ -3,6 +3,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Album from "../../../../lib/data/dataObjects/Album";
 import { useState } from "react";
 import Photo from "../../../../lib/data/dataObjects/Photo";
+import PhotosList from "./photos-list";
 
 type AlbumItemProps = {
   album: Album;
@@ -11,31 +12,12 @@ type AlbumItemProps = {
 export default function AlbumItem({ album }: AlbumItemProps) {
   if (!album) return <></>;
 
-  const [photos, setPhotos] = useState<Photo[]>([]);
   const [showPhotos, setShowPhotos] = useState<Boolean>(false);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
 
   const handleShowPhotos = () => {
     setShowPhotos((show) => !show);
-    if (showPhotos) fetchPhotos();
   };
 
-  const fetchPhotos = async () => {
-    try {
-      album.photos(page).then((newPhotos) => {
-        if (newPhotos.length === 0) {
-          setHasMore(false);
-        }
-        setPhotos((prevPhotos) => [...prevPhotos, ...newPhotos]);
-      });
-
-      // Update page number and check if there is more data available
-      setPage((prevPage) => prevPage + 1);
-    } catch (error) {
-      console.error("Error fetching albums:", error);
-    }
-  };
 
   return (
     <ListGroupItem>
@@ -49,44 +31,8 @@ export default function AlbumItem({ album }: AlbumItemProps) {
           </Card.Body>
         </Card>
 
-        {showPhotos && (
-          <InfiniteScroll
-            dataLength={photos.length}
-            next={fetchPhotos}
-            hasMore={hasMore}
-            loader={<h4>Loading...</h4>}
-            endMessage={<p>No more photos to load.</p>}
-          >
-            <Row>
-              {photos.map((photo) => (
-                <Col key={photo.id} md={4}>
-                  <img
-                    src={photo.url}
-                    alt={photo.title}
-                    className="img-fluid"
-                  />
-                  <h4>{photo.title}</h4>
-                </Col>
-              ))}
-            </Row>
-          </InfiniteScroll>
-        )}
+        {showPhotos && <PhotosList album={album} showPhotos={showPhotos}/> }
       </div>
     </ListGroupItem>
   );
 }
-
-// <Card className="mt-3">
-//             <Card.Body>
-//               <Card.Title>Photos</Card.Title>
-//               <ListGroup>
-//                 {photos.map((photo) => (
-//                   <ListGroup.Item key={photo.id}>
-//                     <img src={photo.url} alt={photo.title} />
-//                     <h2>{photo.title}</h2>
-//                     <p>URL: {photo.thumbnailUrl}</p>
-//                   </ListGroup.Item>
-//                 ))}
-//               </ListGroup>
-//             </Card.Body>
-//           </Card>
