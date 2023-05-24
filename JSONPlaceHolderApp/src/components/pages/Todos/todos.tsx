ab32537b55b7f8a9881c84c32fffafb43dde314c
AvielBerko <9155988@gmail.com>
2023-05-24 11:35:39 +0300

    Added sort and filter options

--- JSONPlaceHolderApp/src/components/pages/Todos/todos.tsx
+++ JSONPlaceHolderApp/src/components/pages/Todos/todos.tsx
@@ -1,7 +1,20 @@
 import { Container, Row, Col } from "react-bootstrap";
 import TodosList from "./child-components/todos-list";
+import { useState } from "react";
 
 export default function Todos() {
+
+  const [sortBy, setSortBy] = useState('id');
+  const [filterBy, setFilterBy] = useState('id');
+
+  const handleSortOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(event.target.value);
+  };
+
+  const handleFilterOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterBy(event.target.value);
+  };
+
   return (
     <Container>
       <Row className="text-center">
@@ -11,7 +24,24 @@ export default function Todos() {
       </Row>
       <Row>
         <Col>
-          <TodosList />
+        <div className="d-flex flex-row gap-2">
+          <label>Sort By</label>
+          <select value={sortBy} onChange={handleSortOptionChange}>
+            <option value="id">ID</option>
+            <option value="name">Name</option>
+          </select>
+          <label>Filter</label>
+          <select value={filterBy} onChange={handleFilterOptionChange}>
+            <option value="none">None</option>
+            <option value="done">Done</option>
+            <option value="notDone">Not Done</option>
+          </select>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <TodosList sortBy={sortBy} filterBy={filterBy}/>
         </Col>
       </Row>
     </Container>
